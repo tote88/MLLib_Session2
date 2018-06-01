2465e82d6bd5f3906bc5009542db36ec8b0a063d
Jordi Casanovas <tote88@gmail.com>
2018-06-01 20:15:34 +0200

    fins el 6

--- src/main/java/exercise_2/exercise_2.java
+++ src/main/java/exercise_2/exercise_2.java
@@ -28,37 +28,89 @@ public class exercise_2 {
 
 		Dataset<Row> categorised = categorise(withoutNA);
 
-		categorised.show();
+		Dataset<Row> barthel = categoriseBarthel(categorised);
+
+		Dataset<Row> pfeiffer = categorisePfeiffer(barthel);
+
+		Dataset<Row> DifBarthel = categoriseDifBarthel(pfeiffer);
+
+		Dataset<Row> DifPfeiffer = categoriseDifPfeiffer(DifBarthel);
+
+		DifPfeiffer.show();
 
 	    ss.stop(); 
 	}
 
+	private static Dataset<Row> categoriseDifPfeiffer(Dataset<Row> in) {
+		double[] splits =
+				{Double.NEGATIVE_INFINITY, -999, -2, 2, Double.POSITIVE_INFINITY};
+		Bucketizer bucketizer = new Bucketizer()
+				.setInputCol("DiferenciaPfeiffer") //To be removed (?)
+				.setOutputCol("DifPfeifferD")
+				.setSplits(splits);
+
+		return bucketizer.transform(in);
+	}
+
+	private static Dataset<Row> categoriseDifBarthel(Dataset<Row> in) {
+		double[] splits =
+				{Double.NEGATIVE_INFINITY, -999, -20, 20, Double.POSITIVE_INFINITY};
+		Bucketizer bucketizer = new Bucketizer()
+				.setInputCol("DiferenciaBarthel") //To be removed (?)
+				.setOutputCol("DifBarthelD")
+				.setSplits(splits);
+
+		return bucketizer.transform(in);
+	}
+
+	private static Dataset<Row> categorisePfeiffer(Dataset<Row> barthel) {
+		double[] splits =
+				{Double.NEGATIVE_INFINITY, -999, 2, 4, 8, Double.POSITIVE_INFINITY};
+		Bucketizer bucketizer = new Bucketizer()
+				.setInputCol("Pfeiffer") //To be removed (?)
+				.setOutputCol("PfeifferD")
+				.setSplits(splits);
+
+		return bucketizer.transform(barthel);
+	}
+
+	private static Dataset<Row> categoriseBarthel(Dataset<Row> categorised) {
+		double[] splits =
+				{Double.NEGATIVE_INFINITY, -999, 20, 61, 91, 99, Double.POSITIVE_INFINITY};
+		Bucketizer bucketizer = new Bucketizer()
+				.setInputCol("Barthel") //To be removed (?)
+				.setOutputCol("BarthelD")
+				.setSplits(splits);
+
+		return bucketizer.transform(categorised);
+	}
+
 	private static Dataset<Row> categorise(Dataset<Row> withoutNA) {
 		// Hemoglobina
-		double[] splits =
-				{Double.NEGATIVE_INFINITY, -1000, 12.0, Double.POSITIVE_INFINITY};
+		double[] splitHemoglobina =
+				{Double.NEGATIVE_INFINITY, -999, 12.0, Double.POSITIVE_INFINITY};
 		Bucketizer bucketizer = new Bucketizer()
 				.setInputCol("Hemoglobina") //To be removed (?)
 				.setOutputCol("HemoglobinaD")
-				.setSplits(splits);
+				.setSplits(splitHemoglobina);
 
 		Dataset<Row> step1 = bucketizer.transform(withoutNA);
 
-		double[] splits2 =
-				{Double.NEGATIVE_INFINITY, -1000, 1.11, Double.POSITIVE_INFINITY};
+		double[] splitsCreatinina =
+				{Double.NEGATIVE_INFINITY, -999, 1.11, Double.POSITIVE_INFINITY};
 		Bucketizer bucketizer2 = new Bucketizer()
 				.setInputCol("Creatinina") //To be removed (?)
 				.setOutputCol("CreatininaD")
-				.setSplits(splits2);
+				.setSplits(splitsCreatinina);
 
 		Dataset<Row> step2 = bucketizer2.transform(step1);
 
-		double[] splits3 =
-				{Double.NEGATIVE_INFINITY, -1000, 3.5, 5.0, Double.POSITIVE_INFINITY};
+		double[] splitsAlbumina =
+				{Double.NEGATIVE_INFINITY, -999, 3.5, 5.0, Double.POSITIVE_INFINITY};
 		Bucketizer bucketizer3 = new Bucketizer()
 				.setInputCol("Albumina") //To be removed (?)
 				.setOutputCol("AlbuminaD")
-				.setSplits(splits3);
+				.setSplits(splitsAlbumina);
 
 		Dataset<Row> step3 = bucketizer3.transform(step2);
 
